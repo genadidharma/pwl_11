a5931f97db52201e12bc991ce0e08d142648899c
Genadi Dharma <genadislawa@gmail.com>
2021-04-20 12:13:47 +0800

    prak 2 - Handle for json renderer added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Facade\FlareClient\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response as HttpResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +40,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ModelNotFoundException && $request->wantsJson()){
+            return response()->json(
+                ['message' => 'Not Found!'],
+                HttpResponse::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
     }
 }
